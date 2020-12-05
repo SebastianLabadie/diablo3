3ad2da09749f6570df9469ddc114e93f6e6d3921
SebastianLabadie <sebastianlabadie.contact@gmail.com>
2020-12-05 16:43:30 -0300

    home title and forms

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="home">
-    <h1>Home Page</h1>
+    <HomeTitle />
+    <HomeForm/>
   </div>
 </template>
 
 <script>
+import HomeTitle from './HomeTitle'
+import HomeForm from './HomeForm'
+
 export default {
-  name: "Home"
+  name: "Home",
+  components:{HomeTitle,HomeForm}
 };
 </script>
